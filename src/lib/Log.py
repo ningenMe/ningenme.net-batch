1f669db6ce086b995f0f44cd8bf3387de73070d7
kuru <amohkuru@gmail.com>
2020-03-30 02:24:43 +0900

    add slack notification

--- src/lib/Log.py
+++ src/lib/Log.py
@@ -1,6 +1,11 @@
+import sys
 import datetime
 import pathlib
+import requests
+import json
 path = pathlib.Path(__file__).resolve()
+sys.path.append(str(path.parent)+'/../conf')
+import Env
 
 def write(log_level,message):
     now      = datetime.datetime.now()
@@ -11,8 +16,21 @@ def write(log_level,message):
     file.write(message)
     file.close()
 
+def slack(channel,message):
+    url = Env.SLACK_WEBHOOK_URL
+    requests.post(url, data = json.dumps({
+        "channel": channel,
+        "text"   : message
+    }));
+
+
 def info(message=""):
     write("INFO",message)
+    slack("#log-info",message)
 
 def error(message=""):
     write("ERROR",message)
+    slack("#log-error",message)
+
+if __name__ == "__main__":
+    info("テスト")
