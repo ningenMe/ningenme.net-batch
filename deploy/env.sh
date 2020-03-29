213bad5047123ec4fb6aafef33ea17a988a1bbb3
kuru <amohkuru@gmail.com>
2020-03-30 01:58:45 +0900

    modify

--- deploy/env.sh
+++ deploy/env.sh
@@ -2,5 +2,5 @@ file=/home/ec2-user/prod-batch/.env
 : > $file
 
 SLACK_WEBHOOK_URL=`aws ssm get-parameters --name 'slack_webhook_url' --query "Parameters[*].{Value: Value}" --output text`
-export SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
 echo "SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL" >> $file
+export SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
